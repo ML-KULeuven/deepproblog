635ee6a4b9f622b97d0fc3f6c2ebac47c33022a9
Q_D <56118785+Quinten-D@users.noreply.github.com>
2023-10-11 13:33:36 +0200

    Update conf.py

--- src/docs/source/conf.py
+++ src/docs/source/conf.py
@@ -51,6 +51,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+RTD_NEW_THEME = True
 html_theme = "default"
 # html_theme = 'furo'
 # html_static_path = ['_static']
